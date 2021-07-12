90f19bde82c1437d350affe02bfa710f322908af
Lucas Freitas <lucaslcfjf@hotmail.com>
2021-07-12 17:03:07 -0300

    Adiciona cabeçalio aos arquivos

--- src/Core/Analizador.py
+++ src/Core/Analizador.py
@@ -1,3 +1,9 @@
+# Trabalho de Aspectos Teóricos da Computação
+#
+# Lucas Lino do Carmo Freitas   201876034
+# Rodrigo Torres Rego           201876029
+# Wiliam Rocha dos Santos       201876031
+
 from src.Utils.Logs import Logs
 from src.Core.Tags import Tags
 
